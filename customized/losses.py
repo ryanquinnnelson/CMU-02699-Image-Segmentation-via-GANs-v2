Add documentation, fix bug in triplet_loss 2 calculation

customized/losses.py
@@ -255,11 +255,14 @@ def _calculate_loss2(triplet_values, margin=0.2):
     # expects lists of (n,1) shape tensors as input, where n is the number of triplet pairs
 
     # calculating euclidean distance on scalar values means we don't need to square then take square root
-    # (anchor - positive) - (anchor - negative) + margin === negative - positive + margin
+    # we can simply take the absolute value of (anchor - positive) or (anchor - negative)
+    # note that this is different from simply (anchor - positive) - (anchor - negative) because squaring each
+    # term means that each term must be positive
+    # sqrt((anchor - positive)^2) - sqrt((anchor - negative)^2) + margin
 
     # unpack
     anchor_values, negative_values, positive_values = triplet_values
-    option1 = negative_values - positive_values + margin
+    option1 = np.abs(anchor_values - positive_values) - np.abs(anchor_values - negative_values) + margin
     option1 = option1.cpu()  # move smaller tensor now rather than entire output image
     option2 = torch.zeros(option1.shape)
 

tests/test_losses.py
@@ -516,13 +516,24 @@ def test__get_triplet_values3__1d_two_indices(outputs_1d):
         np.testing.assert_equal(actual_positive_values[i].numpy(), expected_positive_values[i].numpy())
 
 
+def test__calculate_loss__two_triplet_pairs():
+    anchor_values = [torch.tensor([0.0]),torch.tensor([0.0])]
+    negative_values = [torch.tensor([2.0]), torch.tensor([1.0])]
+    positive_values = [torch.tensor([0.0]), torch.tensor([2.0])]
+    triplet_values = anchor_values, negative_values, positive_values
+
+    expected = 1.2
+    actual = l._calculate_loss(triplet_values)
+    assert actual == pytest.approx(expected, rel=1e-3)
+
+
 def test__calculate_loss2__two_triplet_pairs():
     anchor_values = torch.tensor([0.0, 0.0])
     negative_values = torch.tensor([2.0, 1.0])
     positive_values = torch.tensor([0.0, 2.0])
     triplet_values = anchor_values, negative_values, positive_values
 
-    expected = 2.2
+    expected = 1.2
     actual = l._calculate_loss2(triplet_values)
     assert actual == pytest.approx(expected, rel=1e-3)
 